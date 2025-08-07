c0171da482eb78c6a8663a54093e64cb66d1e5ee
Daniel Marin <danfelmarin@gmail.com>
2025-08-06 22:36:18 -0500

    comment knowledge base stack

--- ragApi/app.py
+++ ragApi/app.py
@@ -4,13 +4,13 @@ import os
 import aws_cdk as cdk
 
 from rag_api.rag_api_stack import RagApiStack
-from rag_api.knowledge_base_stack import KnowledgeBaseStack
+#from rag_api.knowledge_base_stack import KnowledgeBaseStack
 
 
 app = cdk.App()
 
 # Create stacks
 rag_stack = RagApiStack(app, "RagApiStack")
-knowledge_base_stack = KnowledgeBaseStack(app, "KnowledgeBaseStack")
+#knowledge_base_stack = KnowledgeBaseStack(app, "KnowledgeBaseStack")
 
 app.synth()
